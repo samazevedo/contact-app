2850987ff51a184b625a5e381169536bcbc40054
sam-azevedo <samuel.azevedo@live.com>
2021-08-02 12:00:02 -0300

    add list contacts

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,33 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
+import ListContacts from './ListContacts'
+
+const contacts = [
+    {
+        "id": "michael",
+        "name": "Michael Jackson",
+        "handle": "michaeljackson",
+        "avatarURL": "http://localhost:5001/michaeljackson.jpg"
+    },
+    {
+        "id": "beyonce",
+        "name": "Beyonce Knowles Carter",
+        "handle": "beyonce",
+        "avatarURL": "http://localhost:5001/beyonce.jpg"
+    },
+    {
+        "id": "freddie",
+        "name": "Freddie Mercury",
+        "handle": "freddiemercury",
+        "avatarURL": "http://localhost:5001/freddiemercury.jpg"
+    },
+
+]
 
 class App extends Component {
-  render() {
-    return (
-      <div>
-        Hello World
-      </div>
-    );
-  }
+    render() {
+        return (
+            <ListContacts contacts={contacts} />
+        )
+    }
 }
-
-export default App;
+export default App
