3b8755cea41a217c52a97d973befb99c4b7f8054
sam-azevedo <samuel.azevedo@live.com>
2021-08-14 01:34:08 -0300

    removed data from this.state added contactsapi file and updated the app to fetch data using componentdidmount method also remove method from contactapi

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,45 +1,34 @@
 import React, { Component } from 'react'
 import ListContacts from './ListContacts'
-
-
+import * as ContactsAPI from './utils/ContactsAPI'
 
 class App extends Component {
     state = {
-        contacts: [
-            {
-                "id": "michael",
-                "name": "Michael Jackson",
-                "handle": "michaeljackson",
-                "avatarURL": "http://localhost:5001/michaeljackson.jpg"
-            },
-            {
-                "id": "beyonce",
-                "name": "Beyonce Knowles Carter",
-                "handle": "beyonce",
-                "avatarURL": "http://localhost:5001/beyonce.jpg"
-            },
-            {
-                "id": "freddie",
-                "name": "Freddie Mercury",
-                "handle": "freddiemercury",
-                "avatarURL": "http://localhost:5001/freddiemercury.jpg"
-            },
-
-        ]
+        contacts: [],
+    }
+    componentDidMount() {
+        ContactsAPI.getAll().then((contacts) => {
+            this.setState(() => ({
+                contacts,
+            }))
+        })
     }
 
     removeContact = (contact) => {
         this.setState((currentState) => ({
             contacts: currentState.contacts.filter((c) => {
                 return c.id !== contact.id
-            })
+            }),
         }))
+
+        ContactsAPI.remove(contact)
     }
     render() {
         return (
             <ListContacts
                 contacts={this.state.contacts}
-                onDeleteContact={this.removeContact} />
+                onDeleteContact={this.removeContact}
+            />
         )
     }
 }
