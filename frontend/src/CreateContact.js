f7d485b69a6268ca728729e6af28a5b442af08fb
sam-azevedo <samuel.azevedo@live.com>
2021-08-16 12:49:22 -0300

    build out the contact form

--- frontend/src/CreateContact.js
+++ frontend/src/CreateContact.js
@@ -1,8 +1,31 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
+import backIcon from './icons/arrow-left.svg'
+import ImageInput from './ImageInput'
+import account from './icons/account.svg'
 
 class CreateContact extends Component {
     render() {
-        return <div>Create Contact</div>
+        return (
+            <div>
+                <Link className='close-create-contact' to='/'>
+                    <img src={backIcon} alt='back arrow' />
+                </Link>
+                <form className='create-contact-form'>
+                    <img src={account} alt='account icon' />
+                    <ImageInput
+                        className='create-contact-avatar-input'
+                        name='avatarURL'
+                        maxHeight={64}
+                    ></ImageInput>
+                    <div className='create-contact-details'>
+                        <input type='text' name='name' placeholder='Name' />
+                        <input type='text' name='handle' placeholder='Handle' />
+                        <button>Add Contact</button>
+                    </div>
+                </form>
+            </div>
+        )
     }
 }
 export default CreateContact
